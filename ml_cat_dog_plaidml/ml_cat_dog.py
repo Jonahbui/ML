86e630ee5423307d39b0d3e26295616296131e35
Jonah <jbui12@yahoo.com>
2020-04-08 20:06:28 -0500

    Updated ml_cat_dog files adding new comments, removing redundant lines of code, and adding a proper README

--- ml_cat_dog_plaidml/ml_cat_dog.py
+++ ml_cat_dog_plaidml/ml_cat_dog.py
@@ -1,46 +1,49 @@
 # Author: Jonah Bui
 # Date: 3/10/2020
-# Best validation accuracy: 0.9940
-# Supress tensorflow cuda warning
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# Description: Used to create and train a CNN model
+# Best validation accuracy: 0.9941
+# Changelog:
+# 4/8/2020 - Removed unecessary packages, added more comments
 
-# Enable GPU support using plaidml
 from plaidml import keras
-os.environ['KERAS_BACKEND'] = "plaidml.keras.backend"
-
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as pltimg
-from PIL import Image
-#import tensorflow as tf
-#from tensorflow import keras
-
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 
+# Enable GPU support using plaidml
+os.environ['KERAS_BACKEND'] = "plaidml.keras.backend"
+
+#--------------------------------------------------------------------------------------------------
+# Input
+#--------------------------------------------------------------------------------------------------
 # Directory containing training data (subdirectories: cat, dog)
 TRAIN_DIR = 'train'
+
+# Model hyperparameters
+target_size=(128,128)
+batch_size=128
 
 # Get images from directory to feed into model and label them based of subdirectory name
 data_gen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
 train_data_gen = data_gen.flow_from_directory(
     TRAIN_DIR,
-    target_size=(128,128),
-    batch_size=128,
+    target_size=target_size,
+    batch_size=batch_size,
     class_mode='binary',
     interpolation='bilinear')
 
 val_data_gen = data_gen.flow_from_directory(
     TRAIN_DIR,
-    target_size=(128,128),
-    batch_size=128,
+    target_size=target_size,
+    batch_size=batch_size,
     class_mode='binary',
     interpolation='bilinear'
 )
 
+#--------------------------------------------------------------------------------------------------
+# Define Model Architecture
+#--------------------------------------------------------------------------------------------------
 # Shape of input images
 input_shape = (128, 128, 3)
 
@@ -48,9 +51,10 @@ input_shape = (128, 128, 3)
 model = Sequential([
     Conv2D(32, kernel_size = (3,3), input_shape = input_shape, padding='same'),
     
-    # Used to reduce size of input images
+    # MaxPooling2D used to reduce size of input images
     MaxPooling2D(pool_size = (2,2)),
-
+    
+    # Conv2D used to extract details from an image
     Conv2D(64, kernel_size = (3,3)),
     MaxPooling2D(pool_size = (2,2)),
 
@@ -67,20 +71,24 @@ model = Sequential([
     # Prevent overfitting the model, by making each node do 'work'
     Dropout(0.3),
 
-    # Fire if dog, not if cat
-    # Sigmoid since binary classification
+    # 0 if cat, 1 if dog
+    # Sigmoid for binary classification
     Dense(1, activation = 'sigmoid')
 ])
 model.summary()
 
-# Compiles the model
+#--------------------------------------------------------------------------------------------------
+# Compile Model
+#--------------------------------------------------------------------------------------------------
 model.compile(
     optimizer = 'adam',
     loss = 'binary_crossentropy',
     metrics = ['accuracy']
 )
 
-# Train the model
+#--------------------------------------------------------------------------------------------------
+# Train Model
+#--------------------------------------------------------------------------------------------------
 model.fit_generator(
     train_data_gen,
     epochs = 15, 
@@ -89,5 +97,7 @@ model.fit_generator(
     validation_steps=196
 )
 
-# Save the model
+#--------------------------------------------------------------------------------------------------
+# Output
+#--------------------------------------------------------------------------------------------------
 model.save('ml_cat_dog.h5')
--- ml_cat_dog_plaidml/ml_cat_dog_test.py
+++ ml_cat_dog_plaidml/ml_cat_dog_test.py
@@ -1,10 +1,10 @@
-# Supress tensorflow cuda warning
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
+# Author: Jonah Bui
+# Date: 3/10/2020
+# Description: Used to allow the model to predict a set of images
+# Best validation accuracy: 0.9941
+# Changelog:
+# 4/8/2020 - Removed unecessary packages, added more comments
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as pltimg
 from PIL import Image
 
 import tensorflow as tf
@@ -12,26 +12,32 @@ from tensorflow import keras
 from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 
+#--------------------------------------------------------------------------------------------------
+# Input
+#--------------------------------------------------------------------------------------------------
 data_gen = ImageDataGenerator(rescale = 1./255)
 
+# Directory to pull images from to predict
+prediction_dir = 'test'
+
 # class_mode is none, since we want the model to predict the labels
-test_gen_data = data_gen.flow_from_directory('test',target_size=(128,128), class_mode=None,interpolation='bilinear')
+test_gen_data = data_gen.flow_from_directory(prediction_dir,target_size=(128,128), class_mode=None,interpolation='bilinear')
 
 labels = ['cat', 'dog']
 
 model = keras.models.load_model('ml_cat_dog.h5')
-
+#--------------------------------------------------------------------------------------------------
+# Model Prediction
+#--------------------------------------------------------------------------------------------------
 prediction = model.predict(test_gen_data)
 
-
-# Output predictions
+#--------------------------------------------------------------------------------------------------
+# Results
+#--------------------------------------------------------------------------------------------------
+# Prints the prediciton of all the input images
 for pred in prediction:
     label = round(pred[0]).astype(int)
     if label == 1:
         print("Dog")
     else:
         print("Cat")
-
-for image in test_gen_data[0]:
-    plt.imshow(image)
-    plt.show()
